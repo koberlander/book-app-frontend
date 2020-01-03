cb075dcfa3b2478de16bdd012a3e73399d0c84ae
Kristin M. Oberlander <koberlander115@gmail.com>
2020-01-03 16:29:39 -0500

    Add fetch call to load books array

--- src/Containers/BooksContainer.js
+++ src/Containers/BooksContainer.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 class BooksContainer extends React.Component {
 
+  state = {
+    books: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/books/')
+    .then(res => res.json())
+    .then(books => {
+      this.setState({
+      })
+    })
+  }
 
   render(){
     return(
