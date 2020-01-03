cc9cfadbf5b1c4b2fdd0f7b868b15860ceaac1ad
Kristin M. Oberlander <koberlander115@gmail.com>
2020-01-03 15:53:09 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BooksContainer from './Containers/BooksContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Books! Books! Books!
+      </header>
+      <body>
+        <BooksContainer />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/BooksContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class BooksContainer extends React.Component {
+
+
+  render(){
+    return(
+      <div>Books Container</div>
+    )
+  }
+}
+
+export default BooksContainer
